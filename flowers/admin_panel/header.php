f0e3b53be34735bafe8116830f76c8f4c016d87c
cybb <cybb7@yahoo.com>
2016-10-11 17:53:48 +0300

    admin

---
+++ flowers/admin_panel/header.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Административный раздел</title> 
+    <?php include "../config.php"; ?>
+    <link rel="stylesheet" href="../styles/bootstrap-grid-3.3.1.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/media.css">
+</head>
+<body>
+  <div class="container">
+      <div class="row">
+          <div class="header col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <h1>Административный раздел</h1> 
+          </div>
+      </div>
